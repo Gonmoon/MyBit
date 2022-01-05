b58d20e750a6c2b35f7842e71728af0729773d51
GodOfMoon <86917638+Gonmoon@users.noreply.github.com>
2022-01-05 17:43:28 +0300

    addPointers

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,19 +3,19 @@ import json
 
 
 class bot():
-    def __init__(self, bot_token):
+    def __init__(self, bot_token: str):
         # -----создание url бота-----
         self.url = 'https://api.telegram.org/bot' + bot_token
         self.bot_token = bot_token
            
-    def get_updates(self, offset=None):
+    def get_updates(self, offset=None) -> dict:
         # -----получения обновления-----
         url = self.url + "/getUpdates?timeout=100"
         if offset:
             url = url + "&offset={}".format(offset + 1)
         return json.loads(requests.get(url).content)
     
-    def get_chat_id(self):
+    def get_chat_id(self) -> int:
         # -----получения id чата-----
         id = None
         answer = json.loads(requests.get(self.url + '/getUpdates').content)
@@ -24,18 +24,17 @@ class bot():
             id = item["message"]["chat"]["id"]
         return id
     
-    def send_message(self, text):
+    def send_message(self, text: str):
         # -----отправка сообщения-----
         requests.post('{0}/sendMessage?chat_id={1}&text={2}'.format(self.url,
         bot(self.bot_token).get_chat_id(), text))
 
-    def send_message_with_json(self, text, json):
+    def send_message_with_json(self, text: str, json: dict):
         # -----отправка сообщения с json-----
         requests.post('{0}/sendMessage?chat_id={1}&text={2}'.format(self.url,
         bot(self.bot_token).get_chat_id(), text), json=json)
 
-    def send_sticker(self, sticker):
+    def send_sticker(self, sticker: str):
         # -----отправка стикера-----
         requests.post('{0}/sendSticker?chat_id={1}&sticker={2}'.format(self.url,
         bot(self.bot_token).get_chat_id(), sticker))
-     
